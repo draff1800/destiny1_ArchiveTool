bada248cffc89521778efc01d66d6bde02969b84
Dylan Rafferty <draff1800@gmail.com>
2020-05-22 21:58:23 +0100

    Welcome message and button. Added simple jest snapshot test.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Button, Typography } from 'antd'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Typography.Title level={1} style={styles.title}>
+          We need your permission...
+        </Typography.Title>
+        <Typography.Text style={styles.text}>
+          Allow DAT to view your Destiny characters.
+        </Typography.Text>
+        <Button type="primary" size="large" style={styles.button}>Authorise with Bungie.net</Button>
       </header>
     </div>
   );
+}
+
+const styles = {
+  title: {color: "white", fontWeight: "normal", marginBottom: 5},
+  text: {fontSize: 18, color: "white", marginBottom: 20},
+  button: {backgroundColor: "orange", color: "#353535", fontWeight: "bold"}
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
+// Link.react.test.js
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Check that the page renders', () => {
+  const component = renderer.create(
+    <App></App>,
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
